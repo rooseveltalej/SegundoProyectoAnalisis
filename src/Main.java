67adb328352af92637fc21730aa07ad6bdd56dd3
Lorenz Machine <111193109+MachineJALS@users.noreply.github.com>
2024-05-25 13:26:22 -0600

    Prueba con 10 items
    Revisar la manera de poner un valor fijo a los items para poder calcular si lo esta haciendo correctamente

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,8 @@ public class Main {
             new Item("Item1", 2, 3),
             new Item("Item2", 3, 4),
             new Item("Item3", 4, 5),
-            new Item("Item4", 5, 7)
+            new Item("Item4", 5, 7),
+            new Item("Item5",6,15)
         };
 
         Mochila mochila = new Mochila(6, items);
@@ -23,6 +24,29 @@ public class Main {
         System.out.println("Resultado usando Algoritmo Genético: " + resultadoGA);
         System.out.println("Items seleccionados: " + 
             Arrays.stream(resultadoGA.getItems()).map(Item::getNombre).toList());
+             //Prueba 5 items
+        
+
+        Item[] items2 = {};
+        for (int i = 0; i < 10; i++) {
+            items2 = Arrays.copyOf(items2, items2.length + 1);
+            items2[items2.length - 1] = new Item("Item" + (i + 1), i + 1, i + 1);
+        }
+
+        Mochila mochila2 = new Mochila(10, items2);
+
+        //Resolver usando programación dinámica
+        Mochila resultadoDP2 = Dinamico.resolverMochilaDP(mochila2);
+        System.out.println("Resultado usando Programación Dinámica: " + resultadoDP2);
+        System.out.println("Items seleccionados: " + 
+            Arrays.stream(resultadoDP2.getItems()).map(Item::getNombre).toList());
+
+        //Resolver usando algoritmo genético
+        Mochila resultadoGA2 = Genetico.resolverMochilaGA(mochila2);
+        System.out.println("Resultado usando Algoritmo Genético: " + resultadoGA2);
+        System.out.println("Items seleccionados: " + 
+            Arrays.stream(resultadoGA2.getItems()).map(Item::getNombre).toList());
+            
     }
 } //Hola mundo
 
